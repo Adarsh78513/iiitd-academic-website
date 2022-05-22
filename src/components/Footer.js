87f1e6d7194a73482469bbb18016255795059e8b
Nishaant-Rastogi <nishaant20091@iiitd.ac.in>
2022-05-22 23:49:13 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import '../styles/footer.css'
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="footer">
+            <div className="content">
+                Copyright © 2021 IIIT-Delhi. All rights reserved.
+            </div>
+            <div className="icons">
+
+            </div>
+        </div>
     )
 }
 
