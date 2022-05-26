48b7fb617746fd4ab281f5c159728a693f794c86
Nishaant-Rastogi <nishaant20091@iiitd.ac.in>
2022-05-26 12:21:33 +0530

    convocations

--- src/pages/Convocation.js
+++ src/pages/Convocation.js
@@ -3,6 +3,7 @@ import Marquee from '../components/Marquee'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import AdCarousel from '../components/AdCarousel'
+import '../styles/convocation.css'
 
 const Convocation = () => {
     return (
@@ -11,6 +12,16 @@ const Convocation = () => {
             <Navbar />
             <AdCarousel />
             <div className="heading">Convocation</div>
+            <div className="convocation">
+                <div className="convoCont">
+                    Recently Held 10th Convocation
+                    <img className="prev" src="https://www.iiitd.ac.in/convocation21/assets/images/galleryslider/IMG_3927.jpg" alt="" />
+                </div>
+                <div className="convoCont">
+                    Previous Convocations
+                    <img className="prev" src="https://www.iiitd.ac.in/convocation21/assets/images/galleryslider/IMG_3511.jpg" alt="" />
+                </div>
+            </div>
             <Footer />
         </div>
     )
