9c751a5dc03b317930cef81b95c4b37bc28264cf
Nishaant-Rastogi <nishaant20091@iiitd.ac.in>
2022-05-26 11:39:53 +0530

    academic team

--- src/pages/AcademicTeam.js
+++ src/pages/AcademicTeam.js
@@ -11,26 +11,160 @@ const AcademicTeam = () => {
             <Navbar />
             <div className="heading">Academic Team</div>
             <hr></hr>
-            <div className="dean">
-
+            <div className="containerTeam">
+                <div className="dean">
+                    <div className="subTeam">DoAA, Registrar</div>
+                    <div className="cardContainer">
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">PROF. PUSHPENDRA SINGH</div>
+                                <div className="des">Dean of Academic Affairs(DoAA)</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">DR. ASHOK KUMAR SOLANKI</div>
+                                <div className="des">Registrar</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="adminAcademic">
+                    <div className="subTeam">Admin Academic</div>
+                    <div className="cardContainer">
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">ASHUTOSH BRAHMA</div>
+                                <div className="des">Deputy Manager (Academics)</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">K P SINGH</div>
+                                <div className="des">Consultant (Academics)</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="adminAcademic">
-
+            <div className="containerTeam">
+                <div className="adminBTech">
+                    <div className="subTeam">Admin B.Tech</div>
+                    <div className="cardContainer">
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">NISHA NARWAL</div>
+                                <div className="des">Assistant Manager (Academics)</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">AAKASH GUPTA</div>
+                                <div className="des">Junior Manager (Academics)</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">ASHISH AGGARWAL</div>
+                                <div className="des">Junior Manager (Academics)</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">SUDHANSHU TAMTA</div>
+                                <div className="des">Junior Manager (Academics)</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="adminBTech">
-
+            <div className="containerTeam">
+                <div className="adminMTech">
+                    <div className="subTeam">Admin M.Tech</div>
+                    <div className="cardContainer">
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">ASHUTOSH BRAHMA</div>
+                                <div className="des">Deputy Manager (Academics)</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">MOHIT KUMAR</div>
+                                <div className="des">Junior Manager (Academics)</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="adminPhD">
+                    <div className="subTeam">Admin Ph.D</div>
+                    <div className="cardContainer">
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">ANSHU DUREJA</div>
+                                <div className="des">Deputy Manager (Academics)</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">RAJU BISWAS</div>
+                                <div className="des">Junior Manager (Academics)</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="adminMTech">
-
-            </div>
-            <div className="adminPhD">
-
-            </div>
-            <div className="adminInternal">
-
-            </div>
-            <div className="support">
-
+            <div className="containerTeam">
+                <div className="adminInternational">
+                    <div className="subTeam">Admin International Affairs</div>
+                    <div className="cardContainer">
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">PRACHI MUKHERJEE</div>
+                                <div className="des">Assistant Manager (Academics)</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="support">
+                    <div className="subTeam">Support Staff</div>
+                    <div className="cardContainer">
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">RAJAN</div>
+                                <div className="des">Executive Assistant (AV Support)</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">AKHILESH</div>
+                                <div className="des">Executive Assistant (AV Support)</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="" alt="" />
+                            <div className="designation">
+                                <div className="name">ANKUR</div>
+                                <div className="des">Executive Assistant (Academics)</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
