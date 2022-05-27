0d1a756e20a80ac0e48a99f8c8f96dada7b23285
Nishaant-Rastogi <nishaant20091@iiitd.ac.in>
2022-05-27 11:47:59 +0530

    css changes

--- src/pages/AcademicTeam.js
+++ src/pages/AcademicTeam.js
@@ -16,14 +16,14 @@ const AcademicTeam = () => {
                     <div className="subTeam">DoAA, Registrar</div>
                     <div className="cardContainer">
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://www.iiitd.ac.in/sites/default/files/images/faculty/puspendra.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">PROF. PUSHPENDRA SINGH</div>
                                 <div className="des">Dean of Academic Affairs(DoAA)</div>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/registrar-asolanki.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">DR. ASHOK KUMAR SOLANKI</div>
                                 <div className="des">Registrar</div>
@@ -35,14 +35,14 @@ const AcademicTeam = () => {
                     <div className="subTeam">Admin Academic</div>
                     <div className="cardContainer">
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/asutosh.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">ASHUTOSH BRAHMA</div>
                                 <div className="des">Deputy Manager (Academics)</div>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/kpsingh.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">K P SINGH</div>
                                 <div className="des">Consultant (Academics)</div>
@@ -56,28 +56,28 @@ const AcademicTeam = () => {
                     <div className="subTeam">Admin B.Tech</div>
                     <div className="cardContainer">
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/nisha.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">NISHA NARWAL</div>
                                 <div className="des">Assistant Manager (Academics)</div>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/aakash.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">AAKASH GUPTA</div>
                                 <div className="des">Junior Manager (Academics)</div>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/ashisha.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">ASHISH AGGARWAL</div>
                                 <div className="des">Junior Manager (Academics)</div>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/sudhanshut.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">SUDHANSHU TAMTA</div>
                                 <div className="des">Junior Manager (Academics)</div>
@@ -91,14 +91,14 @@ const AcademicTeam = () => {
                     <div className="subTeam">Admin M.Tech</div>
                     <div className="cardContainer">
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/asutosh.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">ASHUTOSH BRAHMA</div>
                                 <div className="des">Deputy Manager (Academics)</div>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/mohit.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">MOHIT KUMAR</div>
                                 <div className="des">Junior Manager (Academics)</div>
@@ -110,14 +110,14 @@ const AcademicTeam = () => {
                     <div className="subTeam">Admin Ph.D</div>
                     <div className="cardContainer">
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/anshu.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">ANSHU DUREJA</div>
                                 <div className="des">Deputy Manager (Academics)</div>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/raju.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">RAJU BISWAS</div>
                                 <div className="des">Junior Manager (Academics)</div>
@@ -131,7 +131,7 @@ const AcademicTeam = () => {
                     <div className="subTeam">Admin International Affairs</div>
                     <div className="cardContainer">
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/prachi.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">PRACHI MUKHERJEE</div>
                                 <div className="des">Assistant Manager (Academics)</div>
@@ -143,21 +143,21 @@ const AcademicTeam = () => {
                     <div className="subTeam">Support Staff</div>
                     <div className="cardContainer">
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/rajanav.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">RAJAN</div>
                                 <div className="des">Executive Assistant (AV Support)</div>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/akhileshav.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">AKHILESH</div>
                                 <div className="des">Executive Assistant (AV Support)</div>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="" alt="" />
+                            <img className="img" src="https://iiitd.ac.in/sites/default/files/images/admin/staff/ankurav.jpg" alt="" />
                             <div className="designation">
                                 <div className="name">ANKUR</div>
                                 <div className="des">Executive Assistant (Academics)</div>
