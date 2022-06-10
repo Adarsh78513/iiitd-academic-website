01d510bc9919d8a9feab6952330642eef95fefd3
Adarsh78513 <adrsrj@gmail.com>
2022-06-11 00:01:42 +0530

    added imaghe in classroom

--- src/pages/Classrooms.js
+++ src/pages/Classrooms.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/classrooms.css'
 import { ParallaxBanner } from 'react-scroll-parallax';
 
+
 const Classrooms = () => {
   return (
     <ParallaxBanner
@@ -33,19 +34,19 @@ const Classrooms = () => {
       <div className='page classroom'>
         <div className='heading' style={{ marginBottom: "35vh" }}>Classrooms</div>
         <div className="classContainer">
-          <a style={{ textDecoration: 'none' }} href="/classrooms">
+          <a style={{ textDecoration: 'none' }} href='./Old.jpg'>
             <div className="cell">
               <img className="old" src="https://www.highereducationdigest.com/wp-content/uploads/2020/02/IIIT-D-768x461.jpg" alt="" />
               <div>Academic Building</div>
             </div>
           </a>
-          <a style={{ textDecoration: 'none' }} href="/classrooms">
+          <a style={{ textDecoration: 'none' }} href="./Lhc.jpg">
             <div className="cell">
               <img className='lhc' src="https://iiitd.ac.in/gallery/assets/img/Institute/institute19-1.jpg" alt="" />
               <div>Lecture Hall Complex</div>
             </div>
           </a>
-          <a style={{ textDecoration: 'none' }} href="/classrooms">
+          <a style={{ textDecoration: 'none' }} href="./R&d.jpg">
             <div className="cell">
               <img className="lhc" src="https://images.shiksha.com/mediadata/images/articles/1607484338php8JZWhd.jpeg" alt="" />
               <div>R & D Building</div>
